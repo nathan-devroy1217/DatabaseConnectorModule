d980999c799810c0cebb6a36341272b63da8e361
ndevroy <nathan.devroy@optum.com>
2018-08-22 22:40:40 -0500

    first commit

---
+++ src/main/java/com/nathan/DatabaseConnectorModule/DatabaseConnectorModuleApplication.java
@@ -0,0 +1,12 @@
+package com.nathan.DatabaseConnectorModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseConnectorModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseConnectorModuleApplication.class, args);
+	}
+}
